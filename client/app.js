b6e7e38d7cd3fef74ad684386571b1c68b11d7f6
Rony Chung <bluegreenapple.87@gmail.com>
2015-08-12 17:25:29 -0300

    login working for fb and email

---
+++ client/app.js
@@ -0,0 +1,60 @@
+angular.module('MyApp', ['ngResource', 'ngMessages', 'ui.router', 'mgcrea.ngStrap', 'satellizer'])
+  .config(function($stateProvider, $urlRouterProvider, $authProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'partials/home.html'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'partials/login.html',
+        controller: 'LoginCtrl'
+      })
+      .state('signup', {
+        url: '/signup',
+        templateUrl: 'partials/signup.html',
+        controller: 'SignupCtrl'
+      })
+      .state('logout', {
+        url: '/logout',
+        template: null,
+        controller: 'LogoutCtrl'
+      })
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'partials/profile.html',
+        controller: 'ProfileCtrl',
+        resolve: {
+          authenticated: function($q, $location, $auth) {
+            var deferred = $q.defer();
+
+            if (!$auth.isAuthenticated()) {
+              $location.path('/login');
+            } else {
+              deferred.resolve();
+            }
+
+            return deferred.promise;
+          }
+        }
+      });
+
+    $urlRouterProvider.otherwise('/');
+
+    $authProvider.facebook({
+      clientId: '1669660303269862'
+    });
+
+    $authProvider.google({
+      clientId: '631036554609-v5hm2amv4pvico3asfi97f54sc51ji4o.apps.googleusercontent.com'
+    });
+
+    $authProvider.linkedin({
+      clientId: '77cw786yignpzj'
+    });
+
+    $authProvider.twitter({
+      url: '/auth/twitter'
+    });
+    
+  });
