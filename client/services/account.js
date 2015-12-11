2aba1e4ccd67029f41980147cc6c6a4e384cfe1b
Rony Chung <bluegreenapple.87@gmail.com>
2015-12-11 18:47:19 -0200

    retaining user in account

--- client/services/account.js
+++ client/services/account.js
@@ -1,9 +1,18 @@
 angular.module('MyApp')
   .factory('Account', function($http) {
+    var profilePromise;// = $http.get('/api/me');;
+
     return {
       getProfile: function() {
-        return $http.get('/api/me');
+        if ( angular.isDefined( profilePromise ) )
+        {
+          console.log('profile promise exists:',profilePromise);
+         return profilePromise;
+        }
+        console.log('profile promise not defined yet, we lets send a http request!'); 
+        return profilePromise = $http.get('/api/me');
       },
+
       updateProfile: function(profileData) {
         return $http.put('/api/me', profileData);
       }
